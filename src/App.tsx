c388559d4996b04d91d781dcf753502fd9dd412d
AmitGaikar00 <amitgaikar6025@gmail.com>
2024-08-29 19:08:11 +0530

    safari viewport fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Outlet, ScrollRestoration } from "react-router-dom";
 
 const App: React.FC = () => {
+  useEffect(() => {
+    if (navigator.userAgent.indexOf("iPhone") > -1) {
+      const viewport = document.querySelector("[name=viewport]");
+      if (viewport) {
+        viewport.setAttribute(
+          "content",
+          "width=device-width, initial-scale=1, maximum-scale=1"
+        );
+      }
+    }
+  }, []);
 
   return (
     <main className="flex items-center justify-center">
