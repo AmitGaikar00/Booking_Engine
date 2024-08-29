03f2805dac44e409a841a00c627e60b5c5e02fb8
AmitGaikar00 <amitgaikar6025@gmail.com>
2024-08-29 14:27:48 +0530

    createportal for spinner

--- src/components/shared/Spinner.tsx
+++ src/components/shared/Spinner.tsx
@@ -1,11 +1,13 @@
 import { Loader2 } from "lucide-react";
 import React from "react";
+import { createPortal } from "react-dom";
 
 const Spinner: React.FC = () => {
-  return (
+  return createPortal(
     <div className="max-w-2xl mx-auto grid h-screen place-content-center">
       <Loader2 className="mr-2 h-8 w-8 animate-spin text-primary" />
-    </div>
+    </div>,
+    document.body
   );
 };
 
